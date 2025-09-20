ff326910f2600fab9a3961e76e01b06c77ce8c5e
AndyDo0942 <doa12967@gmail.com>
2025-09-20 12:56:31 -0400

    partial DB

--- backend/db.py
+++ backend/db.py
@@ -7,36 +7,10 @@ song_artist = db.Table(
     db.Column('artist_id', db.Integer, db.ForeignKey('artists.id'), primary_key=True)
 )
 
-"""song_genre = db.Table(
-    'song_genre',
-    db.Column('song_id', db.Integer, db.ForeignKey('Songs.id'), primary_key=True),
-    db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
-)"""
 
-<<<<<<< Updated upstream
-=======
-"""class Playlist(db.Model):
-    __tablename__ = "playlists"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    spotify_playlist_id = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-
-    #relationships
-    user = db.relationship('User', back_populates='playlist')
-    songs = db.relationship('Song', back_populates='playlist')
-    recommendations = db.relationship('Recommendation')"""
->>>>>>> Stashed changes
 class Song(db.Model):
     __tablename__ = "Songs"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-<<<<<<< Updated upstream
-    tempo = db.Column(db.Integer, nullable=False)
-=======
     spotify_id = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(200), nullable=False)
     artists = db.Column(db.list, nullable=False)
@@ -55,38 +29,26 @@ class Song(db.Model):
     mode = db.Column(db.Integer, nullable=True)
     time_signature = db.Column(db.Integer, nullable=True)
     
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    
     # One-to-many relationship with playlist
     """playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'), nullable=False)
     playlist = db.relationship('Playlist', back_populates='songs')"""
     
     # Many-to-many relationships
->>>>>>> Stashed changes
+
     artists = db.relationship('Artist', secondary=song_artist, back_populates='songs')
     """genres = db.relationship('Genre', secondary=song_genre, back_populates='songs')"""
 
 class Artist(db.Model):
     __tablename__ = "artists"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-<<<<<<< Updated upstream
     name = db.Column(db.String, nullable=False)
-=======
     spotify_id = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(200), nullable=False)
     genre = db.Column(db.List, nullable=True)
     #Many to many
->>>>>>> Stashed changes
+
     songs = db.relationship('Song', secondary=song_artist, back_populates='artists')
 
-"""class Genre(db.Model):
-    __tablename__ = "genres"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-<<<<<<< Updated upstream
-    songs = db.relationship('Song', secondary=song_genre, back_populates='genres')
-=======
-    songs = db.relationship('Song', secondary=song_genre, back_populates='genres')"""
 
 class Recommendation(db.Model):
     __tablename__ = "recommendations"
@@ -152,4 +114,4 @@ def create_song(spotify_id, name, artist, audio_features=None):
 
     return song
 
->>>>>>> Stashed changes
+
