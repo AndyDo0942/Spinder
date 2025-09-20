b6c60127c0ab11ba4bc1b3bf51a2ef312ab2778f
Anatoli Monsalve <anatolim2007@gmail.com>
2025-09-20 14:47:35 -0400

    Track SwipeTune iOS files as regular files (remove submodule)

---
+++ SwipeTune/SwipeTune/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwipeTune
+//
+//  Created by Anatoli Monsalve on 9/19/25.
+//
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("didOnboard") private var didOnboard = false
+    var body: some View {
+        if didOnboard {
+            SongSwipeHome()    // or RootAppView() if you want the playlist sheet path
+        } else {
+            RootAppView()      // includes the “Import Playlist Link” option
+        }
+    }
+}
+
+#Preview { ContentView() }
